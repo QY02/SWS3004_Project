4e4f4948b5d36b2c7350638a0c7b3abf04519d56
QY02 <1494893742@qq.com>
2024-07-14 22:26:19 +0800

    Add some test data

--- sql/testData.sql
+++ sql/testData.sql
@@ -1,6 +1,25 @@
 insert into brief_event (id, publisher_full_id, publish_datetime, name, detailed_data_location)
 VALUES (1, '3976113110000000', '2024-07-08 22:43:51', 'abc', 0);
 
+insert into seat_map_template(id, name, data)
+VALUES (1, 'aaa', '{
+  "size": {
+    "width": 80,
+    "height": 60
+  },
+  "seats": [{"id": "A1", "x": 0, "y": 0, "color": "#0059de"},
+    {"id": "A2", "x": 10, "y": 0, "color": "#0059de"},
+    {"id": "A3", "x": 20, "y": 0, "color": "#0059de"}
+  ]
+}');
+
+insert into seat_template(seat_map_id, seat_id, type, price)
+VALUES (1, 'A1', 'a', 1);
+insert into seat_template(seat_map_id, seat_id, type, price)
+VALUES (1, 'A2', 'b', 2);
+insert into seat_template(seat_map_id, seat_id, type, price)
+VALUES (1, 'A3', 'c', 3);
+
 insert into event(id, publisher_full_id, publish_datetime, name, content)
 VALUES (1, '3976113110000000', '2024-07-08 22:43:51', 'abc', 'aaabbbccc');
 
